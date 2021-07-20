8c74361827ee4aff09cab6829b2a52c46cec4a3e
wwwkang8 <wwwkang8@gmail.com>
2021-07-20 20:49:14 +0900

    매일경제 부동산뉴스 스크래핑

---
+++ src/main/java/com/yogurt/vienna/controller/NewsScraperController.java
@@ -0,0 +1,28 @@
+package com.yogurt.vienna.controller;
+
+import com.yogurt.vienna.entity.NewsDTO;
+import com.yogurt.vienna.service.NewsScraperService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class NewsScraperController {
+
+    @Autowired
+    NewsScraperService newsScraperService;
+
+    @RequestMapping("/news")
+    public ResponseEntity<?> newsScraping(){
+
+        List<NewsDTO> newsDTOList = new ArrayList<>();
+        newsDTOList = newsScraperService.scrapingNews();
+
+        return ResponseEntity.ok(newsDTOList);
+    }
+
+}
---
+++ src/main/java/com/yogurt/vienna/entity/NewsDTO.java
@@ -0,0 +1,31 @@
+package com.yogurt.vienna.entity;
+
+public class NewsDTO {
+
+    private String newsLink;
+    private String Text;
+
+    public NewsDTO(String newsLink, String text) {
+        this.newsLink = newsLink;
+        Text = text;
+    }
+
+    public NewsDTO() {
+    }
+
+    public String getNewsLink() {
+        return newsLink;
+    }
+
+    public void setNewsLink(String newsLink) {
+        this.newsLink = newsLink;
+    }
+
+    public String getText() {
+        return Text;
+    }
+
+    public void setText(String text) {
+        Text = text;
+    }
+}
