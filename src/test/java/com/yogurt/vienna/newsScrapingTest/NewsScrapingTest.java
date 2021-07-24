9ebddde101b8347f0f72ce2caaf3994d690b9753
wwwkang8 <wwwkang8@gmail.com>
2021-07-24 23:59:34 +0900

    JUnit 테스트 추가

---
+++ src/test/java/com/yogurt/vienna/newsScrapingTest/NewsScrapingTest.java
@@ -0,0 +1,14 @@
+package com.yogurt.vienna.newsScrapingTest;
+
+import com.yogurt.vienna.config.WebSecurityConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class NewsScrapingTest {
+
+    //AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(WebSecurityConfig.class);
+
+
+
+}
