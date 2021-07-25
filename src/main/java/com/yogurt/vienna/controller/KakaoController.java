cddcffa04721880ab9f61edf083a6a43242b2157
wwwkang8 <wwwkang8@gmail.com>
2021-07-25 16:54:38 +0900

    카카오 인가코드 받기 완료

--- src/main/java/com/yogurt/vienna/controller/KakaoController.java
+++ src/main/java/com/yogurt/vienna/controller/KakaoController.java
@@ -1,2 +1,39 @@
-package com.yogurt.vienna.controller;public class KakaoController {
+package com.yogurt.vienna.controller;
+
+import com.yogurt.vienna.entity.KakaoLoginDTO;
+import com.yogurt.vienna.service.KakaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KakaoController {
+
+    @Autowired
+    KakaoService kakaoService;
+
+    @RequestMapping("/kakaoAuth")
+    public void kakaoAuthorizationCode(@RequestParam("code") String code){
+
+        String authCode = code;
+
+        kakaoService.getAccessToken(authCode);
+
+    }
+
+    @RequestMapping("/kakaoLogin")
+    public ResponseEntity<?> kakaoLogin(@RequestParam("access_token") String access_token){
+
+        String token = access_token;
+        KakaoLoginDTO kakaoLoginDTO = new KakaoLoginDTO();
+        kakaoLoginDTO.setAccess_token(token);
+
+        System.out.println("kakaoLogin 맵핑 - 토큰 : " + access_token);
+
+        return ResponseEntity.ok(kakaoLoginDTO);
+
+    }
+
 }
--- src/main/java/com/yogurt/vienna/entity/KakaoLoginDTO.java
+++ src/main/java/com/yogurt/vienna/entity/KakaoLoginDTO.java
@@ -1,2 +1,71 @@
-package com.yogurt.vienna.entity;public class KakaoLoginDTO {
+package com.yogurt.vienna.entity;
+
+public class KakaoLoginDTO {
+
+    private String client_id;
+    private String access_token;
+    private String token_type;
+    private String refresh_token;
+    private String expires_in;
+    private String refresh_token_expires_in;
+
+    public KakaoLoginDTO() {
+    }
+
+    public KakaoLoginDTO(String client_id, String access_token, String token_type, String refresh_token, String expires_in, String refresh_token_expires_in) {
+        this.client_id = client_id;
+        this.access_token = access_token;
+        this.token_type = token_type;
+        this.refresh_token = refresh_token;
+        this.expires_in = expires_in;
+        this.refresh_token_expires_in = refresh_token_expires_in;
+    }
+
+    public String getClient_id() {
+        return client_id;
+    }
+
+    public void setClient_id(String client_id) {
+        this.client_id = client_id;
+    }
+
+    public String getAccess_token() {
+        return access_token;
+    }
+
+    public void setAccess_token(String access_token) {
+        this.access_token = access_token;
+    }
+
+    public String getToken_type() {
+        return token_type;
+    }
+
+    public void setToken_type(String token_type) {
+        this.token_type = token_type;
+    }
+
+    public String getRefresh_token() {
+        return refresh_token;
+    }
+
+    public void setRefresh_token(String refresh_token) {
+        this.refresh_token = refresh_token;
+    }
+
+    public String getExpires_in() {
+        return expires_in;
+    }
+
+    public void setExpires_in(String expires_in) {
+        this.expires_in = expires_in;
+    }
+
+    public String getRefresh_token_expires_in() {
+        return refresh_token_expires_in;
+    }
+
+    public void setRefresh_token_expires_in(String refresh_token_expires_in) {
+        this.refresh_token_expires_in = refresh_token_expires_in;
+    }
 }
