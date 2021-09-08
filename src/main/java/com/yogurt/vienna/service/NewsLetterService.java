7d5e93b9edcfd4175a995308ac536967adc29c59
wwwkang8 <wwwkang8@gmail.com>
2021-09-08 17:40:35 +0900

    부동산 실거래가 호출(개발중)

--- src/main/java/com/yogurt/vienna/service/NewsLetterService.java
+++ src/main/java/com/yogurt/vienna/service/NewsLetterService.java
@@ -7,20 +7,38 @@ import com.sendgrid.SendGrid;
 import com.sendgrid.helpers.mail.Mail;
 import com.sendgrid.helpers.mail.objects.Content;
 import com.sendgrid.helpers.mail.objects.Email;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 
 @Service
 public class NewsLetterService {
+
+    /**
+     * 실거래가 API 사이트 : https://www.data.go.kr/data/15058747/openapi.do
+     * */
+
 
     @Value("${from}")
     private String fromEmail;
 
     @Value("${to}")
     private String toEmail;
+
+    @Value("${APT_TRADE_ENCODING_KEY}")
+    private String AptTradeEncodingKey;
+
+    @Value("${APT_TRADE_DECODING_KEY}")
+    private String AptTradeDecodingKey;
 
     public void sendNewsLetter(String sendGridApiKey) {
 
@@ -46,5 +64,43 @@ public class NewsLetterService {
         }
 
     }
+
+    public StringBuilder getAptPrice() throws IOException {
+
+        //url
+        StringBuilder urlBuilder = new StringBuilder("http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade");
+        urlBuilder.append("?"+ URLEncoder.encode("ServiceKey", "UTF-8")+"="+AptTradeEncodingKey);
+        urlBuilder.append("&"+URLEncoder.encode("LAWD_CD","UTF-8")+"="+URLEncoder.encode("11110", "UTF-8"));
+        urlBuilder.append("&" + URLEncoder.encode("DEAL_YMD","UTF-8") + "=" + URLEncoder.encode("201512", "UTF-8")); /*월 단위 신고자료*/
+        URL url = new URL(urlBuilder.toString());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+
+        System.out.println("Response code : "+conn.getResponseCode());
+
+        BufferedReader rd;
+
+        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            sb.append(line);
+        }
+        rd.close();
+        conn.disconnect();
+        System.out.println(sb.toString());
+
+        return sb;
+
+
+    }
+
+
 
 }
